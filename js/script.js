ef78d3f19ba76230349cc2cb0f273bca3251d688
Юлия Анатольевна <ykosovets@gmail.com>
2020-07-12 21:19:17 +0600

    Сделала подгонку под PP разделов: популярные товары, производители

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-var link_map = document.querySelector('.contacts__map');
-var modal_map = document.querySelector('.modal--map');
-var close_map = document.querySelector('.modal__close-button--map');
-var link_letter = document.querySelector('.button--big-button');
-var modal_letter = document.querySelector('.modal--write-us');
-var close_letter = document.querySelector('.modal__close-button--write-us');
+var linkMap = document.querySelector('.contacts__map');
+var modalMap = document.querySelector('.modal--map');
+var closeMap = document.querySelector('.modal__close-button--map');
+var linkLetter = document.querySelector('.button--big-button');
+var modalLetter = document.querySelector('.modal--write-us');
+var closeLetter = document.querySelector('.modal__close-button--write-us');
 
 /*Открываем модальное окно с интерактивной картой*/
-link_map.addEventListener('click', function(evt) {
+linkMap.addEventListener('click', function(evt) {
   evt.preventDefault();
-  modal_map.classList.add('modal--active');
-})
+  modalMap.classList.add('modal--active');
+});
 /*Закрываем модальное окно с интерактивной картой*/
-close_map.addEventListener('click', function(evt) {
+closeMap.addEventListener('click', function(evt) {
   evt.preventDefault();
-  modal_map.classList.remove('modal--active');
+  modalMap.classList.remove('modal--active');
 });
 /*Открываем форму обратной связи*/
-link_letter.addEventListener('click', function(evt) {
+linkLetter.addEventListener('click', function(evt) {
   evt.preventDefault();
-  modal_letter.classList.add('modal--active');
-})
+  modalLetter.classList.add('modal--active');
+});
 /*Закрываем форму обратной связи*/
-close_letter.addEventListener('click', function(evt) {
+closeLetter.addEventListener('click', function(evt) {
   evt.preventDefault();
-  modal_letter.classList.remove('modal--active');
+  modalLetter.classList.remove('modal--active');
 });
