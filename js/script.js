fc9c6dcaea7b35fd897389eaf6d8ebcd814682dd
Юлия Анатольевна <ykosovets@gmail.com>
2020-07-15 12:04:10 +0600

    Исправила замечание по критерию Б1. А именно, добавила модальную форму с корзиной на главную страницу

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var linkBuy = document.querySelector('.hidden-buttons__link--buy');
+var modalBuy = document.querySelector('.modal--basket-window');
+var closeBuy = document.querySelector('.modal__close-button--basket-window');
 var linkMap = document.querySelector('.contacts__map');
 var modalMap = document.querySelector('.modal--map');
 var closeMap = document.querySelector('.modal__close-button--map');
@@ -24,4 +27,14 @@ linkLetter.addEventListener('click', function(evt) {
 closeLetter.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalLetter.classList.remove('modal--active');
+});
+/*Открываем модальное окно с сообщением о добавлении товара в корзину*/
+linkBuy.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modalBuy.classList.add('modal--active');
+})
+/*Закрываем модальное окно с сообщением о добавлении товара в корзину*/
+closeBuy.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modalBuy.classList.remove('modal--active');
 });
